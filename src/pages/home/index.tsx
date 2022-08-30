c8b64e3b11c9439a9361442075cfd525c20ead02
kasugasorata98 <jeromykho98@gmail.com>
2022-08-31 02:25:55 +0800

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -63,7 +63,7 @@ const Home = () => {
                 fontSize={{ base: "24px", md: "48px", lg: "60px", xl: "65px" }}
                 color={colors.darkText}
               >
-                {`Software Engineer.`}
+                <TypeAnimation sequence={[2000, "Software Engineer."]} />
               </Text>
               <Text
                 letterSpacing={1}
