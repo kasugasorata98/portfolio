4ab1af886e9959ec64d4c51412cbd9e1f01e2afd
kasugasorata98 <jeromykho98@gmail.com>
2022-12-04 22:43:32 +0800

    added analytics

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import "../styles/globals.css";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Jeromy Kho [Software Engineer]</title>
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
