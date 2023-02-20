3ab104bd9121ba2653ef13e9f12f353f44362056
kasugasorata98 <jeromykho98@gmail.com>
2023-02-21 01:27:24 +0800

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,23 +1,23 @@
 // theme.ts
 
 // 1. import `extendTheme` function
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 import "@fontsource/space-mono";
 import "@fontsource/roboto";
 
 // 2. Add your color mode config
 const config: ThemeConfig = {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
-}
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
 
 // 3. extend the theme
 const theme = extendTheme({
-    ...config,
-    fonts: {
-        heading: 'Space Mono',
-        body: 'Roboto',
-    }
-})
+  ...config,
+  fonts: {
+    heading: "Space Mono",
+    body: "Roboto",
+  },
+});
 
-export default theme
+export default theme;
